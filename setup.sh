0098ce6160c8e508c91761afefdd9606b52706fe
Denisov Mikhail <manwebdev@gmail.com>
2016-11-03 18:23:32 +0300

    - deploy setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 npm install
-pkill node
+ps -ef | grep "node ./consume.js" | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep "node ./app.js" | grep -v grep | awk '{print $2}' | xargs kill -9
 npm run start &
+echo "Backend Pass started"
