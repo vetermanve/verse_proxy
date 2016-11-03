5e9ec1ee6927566cb833f65fd8d54a64cfe314e8
Denisov Mikhail <manwebdev@gmail.com>
2016-11-03 19:20:45 +0300

    - update setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 [ -d logs ] || mkdir logs
-npm install > logs/npm_install.log
+npm install 2>&1 > logs/npm_install.log
 ps -ef | grep "node ./consume.js" | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep "node ./app.js" | grep -v grep | awk '{print $2}' | xargs kill -9
-npm run start >> logs/run.log &
-npm run consume >> logs/consume.log &
+npm run start 2>&1 >> logs/run.log &
+npm run consume 2>&1 >> logs/consume.log &
 echo "Backend Pass started"
