b01fd0ff0a3e0def4f7f03f7110b760e2c24e6ae
Denisov Mikhail <manwebdev@gmail.com>
2017-03-15 14:30:12 +0300

    - start script update

--- start.sh
+++ start.sh
@@ -6,7 +6,8 @@ if [ -f /tmp/bpass.config ]; then
     pm2 stop ${config}; 
 fi
 
-echo "Starting $(pwd)/cluster.json";
+pwd=$(pwd -P);
+echo "Starting ${pwd}/cluster.json";
 rm /tmp/bpass.config
-ln -s $(pwd)/cluster.json /tmp/bpass.config 
+ln -s ${pwd}/cluster.json /tmp/bpass.config 
 pm2 start cluster.json
