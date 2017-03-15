b4b2f34a58a10347a9930355aabc8811912b3109
Denisov Mikhail <manwebdev@gmail.com>
2017-03-15 15:18:30 +0300

    - stopping by config file copy

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-if [ -f /tmp/bpass.config ]; then
+if [ -f /tmp/bpass.config.link ]; then
     config=$(readlink -n /tmp/bpass.config)
-    echo "Stoppping ${config}"
-    pm2 delete ${config}
+    echo "Stoppping ${config} by copy"
+    pm2 delete /tmp/bpass.config
     echo "Stopped ${config}"
 fi
 
 pwd=$(pwd -P);
 echo "Starting ${pwd}/cluster.json";
 rm /tmp/bpass.config
-ln -s ${pwd}/cluster.json /tmp/bpass.config 
+ln -s ${pwd}/cluster.json /tmp/bpass.config.link 
+cp ${pwd}/cluster.json /tmp/bpass.config 
 pm2 start cluster.json
