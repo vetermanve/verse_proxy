772c21b46777c493256a14c4815e7a1d6f4e7e4d
Denisov Mikhail <manwebdev@gmail.com>
2018-02-12 04:45:09 +0300

    - fix restart on new system

--- start.sh
+++ start.sh
@@ -2,36 +2,33 @@
 
 curConfigLink="/srv/www/conf/bpass.config.link"
 
-if [ -f ${curConfigLink} ]; then
-    oldConfigFile=$(readlink -n ${curConfigLink})
-fi
 
 pwd=$(pwd -P);
 
 configDir=${pwd}"/cluster/config/";
 configLink=${configDir}cluster.json;
 curConfig=$(readlink -n ${configLink})
-
-echo ${curConfig} ${oldConfigFile}
+oldConfig=$(readlink -n ${curConfigLink})
 
 if [ ${curConfig}  ]; then
+    echo "We has a config ${curConfig}";
     configPath=${configDir}${curConfig}
-    if [ ${oldConfigFile} ]; then
-        if [ ${configPath} != $(readlink -n ${curConfigLink}) ]; then
-            echo "Starting config ${configPath}";
-            
-            pm2 start ${configPath}
+    if [ ${configPath} != ${oldConfig} ]; then
+        echo "Starting new config ${configPath}";
         
-            echo "Stoppping ${oldConfigFile} ..."
-            pm2 delete ${oldConfigFile}
-            rm ${curConfigLink}
-            ln -s ${configPath} ${curConfigLink} 
-            
-            echo "Stopped ${oldConfigFile}."
-        else
-            echo "Self restart detected."
-            pm2 startOrRestart ${configPath}
-        fi
+        pm2 start ${configPath}
+    
+        if [ ${oldConfig} ]; then
+            echo "Stoppping ${oldConfig} ..."
+            pm2 delete ${oldConfig}
+            echo "Stopped ${oldConfig}."
+        fi;
+        
+        rm ${curConfigLink}
+        ln -s ${configPath} ${curConfigLink} 
+    else
+        echo "Self restart detected."
+        pm2 startOrRestart ${configPath}
     fi
     
   else
